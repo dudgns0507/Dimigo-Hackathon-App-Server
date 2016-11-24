a83f9409674d4a4c1f198b2cf4399bb7ec489335
dudgns0507 <hoon2585@gmail.com>
2016-11-24 14:00:31 +0900

    Create index page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.listen(process.env.PORT || 8080, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
