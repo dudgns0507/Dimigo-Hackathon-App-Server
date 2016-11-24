ba196cefe46b53c3da72ec7566f8e5c1e516d699
dudgns0507 <hoon2585@gmail.com>
2016-11-24 15:54:03 +0900

    app.js createserver

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var http = require('http');
 
-var hostname = 'https://dimigo-hackthon-app-server.herokuapp.com/';
 var port = 3000;
 
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
+}).listen(port, hostname, () => {
+  console.log('Server running at 3000');
 });
