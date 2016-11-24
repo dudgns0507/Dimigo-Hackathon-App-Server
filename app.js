939bb6eccd8953f722703762d076963aaec5b1b9
dudgns0507 <hoon2585@gmail.com>
2016-11-24 13:51:48 +0900

    Edit app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
