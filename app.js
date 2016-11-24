50fd7078bf702be695430beb8f7d143c6c321464
dudgns0507 <hoon2585@gmail.com>
2016-11-24 14:18:50 +0900

    no message

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ var port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+app.get('/as', function(request, response) {
+  response.send('d');
+});
